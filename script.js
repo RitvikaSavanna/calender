4d22845b23fb75419fc6a7c9d127a30875c439d0
Ritvika <ritvikasavanna@gmail.com>
2024-09-13 10:19:38 +0530

    h

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) { // Adjust the value as needed
-        navbar.classList.add('translucent');
-    } else {
-        navbar.classList.remove('translucent');
-    }
-});
